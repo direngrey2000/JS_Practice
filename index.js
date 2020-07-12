9de34ee90d69eaaecf75ef97d47d18a10bc131e8
Jang-Tae-Young <tyjang1020@gmail.com>
2020-07-12 16:47:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const title = document.getElementById("title");
-// document.getElementsByClassName
+const title = document.querySelector("#title");
 
-title.innerHTML = "Hi! From JS";
-title.style.color = 'red';
-document.title ="I control this page !! HAHAHAHAHAHAHA";
+// function handleResize(){
+//     console.log("I have been resized");
+// }
 
-console.dir(document);
+function handleResize(event){
+    console.log(event);
+    // console.log("I have been resized");
+}
+
+window.addEventListener("resize",handleResize);
+
+
+
+
+
+
+// const title = document.getElementById("title");
+// title.innerHTML = "Hi! From JS";
+// title.style.color = 'red';
+// document.title ="I control this page !! HAHAHAHAHAHAHA";
+// console.dir(document);
 
 
 
