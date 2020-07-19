5189d343cc196546c909285a45ee0be71a91776f
Jang-Tae-Young <tyjang1020@gmail.com>
2020-07-20 00:21:15 +0900

    update

--- weather.js
+++ weather.js
@@ -1,4 +1,25 @@
+const weather = document.querySelector(".js-weather");
+const API_KEY = "399771586713b0e8419b3996b55e81be";
 const COORDS = 'coords';
+
+function getWeather(latitude, longitude){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    )
+    .then(function(response){
+        // console.log(response.json());
+        return response.json();
+    })
+    .then(function(json){
+        // console.log(json);
+
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+
+    });
+
+}
+
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS,JSON.stringify(coordsObj));
@@ -15,6 +36,7 @@ function handleGeoSuccess(position){
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude,longitude);
 }
 
 function handleGeoError(){
@@ -30,7 +52,8 @@ function loadCoords(){
     if(loadedCoords == null){
         askForCoords();
     }else{
-
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 
 }
