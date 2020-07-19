0fd087a18cde1931aa7681418ec207816fa2bc2c
Jang-Tae-Young <tyjang1020@gmail.com>
2020-07-19 23:42:12 +0900

    weather

---
+++ weather.js
@@ -0,0 +1,43 @@
+const COORDS = 'coords';
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS,JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position){
+    // console.log(position);
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj={
+        // latitude: latitude,
+        // longitude: longitude
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+}
+
+function handleGeoError(){
+    console.log('Error!');
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords == null){
+        askForCoords();
+    }else{
+
+    }
+
+}
+
+
+function init(){
+    loadCoords();
+}
+
+init();
